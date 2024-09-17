98085f58b6541d2101aaad8c35880e1c360092a7
AlgoFoe <varun.singh10011@gmail.com>
2024-09-17 15:07:08 +0530

    zoom in and out

--- src/components/TreeVisualizer.tsx
+++ src/components/TreeVisualizer.tsx
@@ -144,78 +144,66 @@ const TreeVisualizer: React.FC = () => {
     setBinaryTree(buildBinaryTreeFromArray(newHeapTree));
   };
 
-  const drawBinaryTree = useCallback(
-    (
-      ctx: CanvasRenderingContext2D,
-      node: Node | null,
-      x: number,
-      y: number,
-      dx: number,
-      dy: number
-    ) => {
-      if (node === null) return;
-      node.x = x;
-      node.y = y;
-
-      const radius = 25;
-      const centerX = x + radius;
-      const centerY = y + radius;
-
-      ctx.lineWidth = 2;
+  const drawBinaryTree = useCallback((
+    ctx: CanvasRenderingContext2D,
+    node: Node | null,
+    x: number,
+    y: number,
+    dx: number,
+    dy: number
+  ) => {
+    if (node === null) return;
+    
+    node.x = x;
+    node.y = y;
+  
+    const radius = 25 * zoomLevel; 
+    const centerX = x + radius;
+    const centerY = y + radius;
+  
+    ctx.lineWidth = 2 * zoomLevel;
+    ctx.beginPath();
+    ctx.fillStyle = "#193145";
+    ctx.strokeStyle = "#3a9bf0";
+  
+    if (node.highlight) ctx.fillStyle = "#3ab1cf";
+  
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  
+    ctx.fillStyle = "white";
+    ctx.font = `${20 * zoomLevel}px Verdana`;
+    ctx.fillText(node.value.toString(), centerX - 12 * zoomLevel, centerY);
+  
+    if (node.left !== null) {
+      const leftX = x - dx;
+      const leftY = y + dy;
+  
       ctx.beginPath();
-      ctx.fillStyle = "#FFA500";
-      ctx.strokeStyle = "green";
-
-      if (node.highlight) ctx.fillStyle = "#3ab1cf";
-
-      ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+      ctx.moveTo(centerX, centerY + radius);
+      ctx.lineTo(leftX + radius, leftY);
+  
+      if (node.left.highlight) ctx.strokeStyle = "#3ab1cf";
       ctx.stroke();
-      ctx.fill();
-
-      ctx.fillStyle = "black";
-      ctx.font = "20px Verdana";
-      ctx.fillText(node.value.toString(), centerX - 12, centerY);
-
-      if (node.left !== null) {
-        const leftX = x - dx + radius;
-        const leftY = y + dy;
-
-        ctx.beginPath();
-        ctx.moveTo(centerX, centerY + radius);
-        ctx.lineTo(leftX, leftY);
-
-        if (node.left.highlight) ctx.strokeStyle = "#3ab1cf";
-        ctx.stroke();
-
-        drawBinaryTree(ctx, node.left, x - dx, y + dy, dx / 2, dy);
-      }
-
-      if (node.right !== null) {
-        const rightX = x + dx + radius;
-        const rightY = y + dy;
-
-        ctx.beginPath();
-        ctx.moveTo(centerX, centerY + radius);
-        ctx.lineTo(rightX, rightY);
-        if (node.right.highlight) ctx.strokeStyle = "#3ab1cf";
-        ctx.stroke();
-
-        drawBinaryTree(ctx, node.right, x + dx, y + dy, dx / 2, dy);
-      }
-    },
-    []
-  );
-
-  // const redrawBinaryTree = useCallback(() => {
-  //   const canvas = canvasRef.current;
-  //   if (canvas) {
-  //     const ctx = canvas.getContext("2d");
-  //     if (ctx) {
-  //       ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //       drawBinaryTree(ctx, binaryTree, canvas.width / 2, 50, 90, 100);
-  //     }
-  //   }
-  // }, [binaryTree, drawBinaryTree]);
+  
+      drawBinaryTree(ctx, node.left, x - dx, y + dy, dx / 2, dy);
+    }
+  
+    if (node.right !== null) {
+      const rightX = x + dx;
+      const rightY = y + dy;
+  
+      ctx.beginPath();
+      ctx.moveTo(centerX, centerY + radius);
+      ctx.lineTo(rightX + radius, rightY);
+      if (node.right.highlight) ctx.strokeStyle = "#3ab1cf";
+      ctx.stroke();
+  
+      drawBinaryTree(ctx, node.right, x + dx, y + dy, dx / 2, dy);
+    }
+  }, [zoomLevel]);
+  
   const redrawBinaryTree = useCallback(() => {
     const canvas = canvasRef.current;
     if (canvas) {
@@ -223,8 +211,14 @@ const TreeVisualizer: React.FC = () => {
       if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.save(); 
-        ctx.setTransform(zoomLevel, 0, 0, zoomLevel, canvas.width / 2 * (1 - zoomLevel), -canvas.height / 2 * (1 - zoomLevel)); 
-        drawBinaryTree(ctx, binaryTree, canvas.width / 2, 50, 80, 80);
+        const centerX = canvas.width / 2;
+        const centerY = canvas.height / 2;
+  
+        ctx.translate(centerX, centerY);
+        ctx.scale(zoomLevel, zoomLevel);
+        ctx.translate(-centerX, -centerY);
+        
+        drawBinaryTree(ctx, binaryTree, centerX, 50 * zoomLevel, 80 * zoomLevel, 80 * zoomLevel);
         ctx.restore(); 
       }
     }
@@ -232,7 +226,7 @@ const TreeVisualizer: React.FC = () => {
   
 
   useEffect(() => {
-    redrawBinaryTree();
+    redrawBinaryTree(); 
   }, [binaryTree, redrawBinaryTree]);
 
   useEffect(() => {
@@ -284,16 +278,16 @@ const TreeVisualizer: React.FC = () => {
     };
   }, [binaryTree, findClickedNode, redrawBinaryTree, resetHighlight]);
   const handleZoomIn = () => {
-    setZoomLevel((prevZoomLevel) => Math.min(prevZoomLevel + 0.1, 2)); // Limit max zoom level
+    setZoomLevel((prevZoomLevel) => Math.min(prevZoomLevel + 0.1, 2));
   };
   
   const handleZoomOut = () => {
-    setZoomLevel((prevZoomLevel) => Math.max(prevZoomLevel - 0.1, 0.5)); // Limit min zoom level
+    setZoomLevel((prevZoomLevel) => Math.max(prevZoomLevel - 0.1, 0.5));
   };
   
   
   return (
-    <div className="App flex flex-col items-center p-4">
+    <div className="flex flex-col items-center p-4">
       <div className="tree-input w-full max-w-lg">
         <input
           type="text"
@@ -352,16 +346,34 @@ const TreeVisualizer: React.FC = () => {
             </span>
           </div>
         </div>
-        <div className="flex gap-2 justify-center mt-2 flex-wrap">
-          <button className="p-2 bg-gray-600 rounded-md" onClick={handleZoomIn}>
-            Zoom In
-          </button>
-          <button className="p-2 bg-gray-600 rounded-md" onClick={handleZoomOut}>
-            Zoom Out
-          </button>
+        <div className="flex gap-2 justify-center mt-4 flex-wrap">
+          <div
+            onClick={handleZoomIn}
+            className="button w-12 h-10 bg-rose-500 rounded-lg cursor-pointer select-none
+            active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+            active:border-b-[0px]
+            transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
+            border-b-[1px] border-x-teal-700"
+          >
+            <span className="flex flex-col justify-center items-center h-full text-white font-bold text-4xl">
+              + 
+            </span>
+          </div>
+          <div
+            onClick={handleZoomOut}
+            className="button w-12 h-10 bg-rose-500 rounded-lg cursor-pointer select-none
+            active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+            active:border-b-[0px]
+            transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
+            border-b-[1px] border-blue-400"
+          >
+            <span className="flex flex-col justify-center items-center h-full text-white font-bold text-4xl">
+              -
+            </span>
+          </div>
         </div>
 
-        <canvas className="mr-5 mb-5 mt-5 -ml-4 max-w-full" ref={canvasRef} />
+        <canvas className="mr-5 mb-5 mt-5 -ml-6 max-w-full" ref={canvasRef} />
       </div>
     </div>
   );
